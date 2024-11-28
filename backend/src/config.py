6f93c995173f8367a456302a473937182fb7e383
Orino1 <orinomohamed@gmail.com>
2024-11-28 23:42:54 +0100

    feat: added JWT cookie configuration and a centralized logging instance - Configured JWT to store tokens in cookies and disabled CSRF protection for cookies at the moment - Added a logger for centralized logging of application events

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from urllib.parse import quote
 
@@ -16,6 +17,18 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_TOKEN_LOCATION = ["cookies"]
+    JWT_COOKIE_CSRF_PROTECT = False
 
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads/thumbnails')
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads/thumbnails")
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+
+logger = logging.getLogger("main")
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler(os.getenv("LOG_FILE"))
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
